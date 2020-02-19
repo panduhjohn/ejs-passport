3786742b8b329615f76f432f8840765cd10c2845
John Stilwell <johnrstilwell@codeimmersives.com>
2020-02-19 15:47:20 -0500

    added main and partials

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', (req, res, next) => {
+    res.render('index');
 });
-
-
-
+router.get('/login', (req, res) => {
+    res.render('main/login');
+});
+router.get('/register', (req, res) => {
+    res.render('main/register');
+});
+router.get('/test', (req, res) => {
+    res.render('test');
+});
 
 module.exports = router;
